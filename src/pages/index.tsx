0cb4c8970c3b62d248802e7275ac64a3f763e1c5
Hamir Mahal <hamirmahal@gmail.com>
2023-10-13 16:15:50 -0700

    feat: track pageviews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import Main from '@/components/Main';
 import Navigation from '@/components/Navigation';
 import { ChakraProvider } from '@chakra-ui/react';
+import mixpanel from 'mixpanel-browser';
+import React from 'react';
 
 export default function Home() {
+  React.useEffect(() => {
+    if (process.env.NEXT_PUBLIC_MIXPANEL_TOKEN) {
+      mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
+        debug: true,
+        track_pageview: true,
+        persistence: 'localStorage',
+      });
+    } else {
+      console.warn('Mixpanel token not found');
+    }
+  }, []);
+
   return (
     <ChakraProvider>
       <Navigation />
